50439447d63dd44785bcc6b6e96432ddecaaf8d3
Alberto Valvidares <45462740+avalvidares@users.noreply.github.com>
2018-12-01 12:55:23 +0100

    Update PublicContentBase.java

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -11,10 +11,17 @@ public class PublicContentBase extends Controller {
         render();
     }
 
-    public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+   public static void processRegister(String username, String password, String passwordCheck, String type){
+        User u = User.loadUser(username);
+        if (u!=null) {
+            flash.put("error", Messages.get("Public.login.error.credentials"));
+            register();
+        }
+        else {
+	        u = new User(username, HashUtils.getMd5(password), type, -1);
+	        u.save();
+	        registerComplete();
+        }
     }
 
     public static void registerComplete(){
